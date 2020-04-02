ab46a18e7b5cc8d698947033144e6d004efb4db2
Haley Rodzos <haley.rodzos@gmail.com>
2020-04-02 01:55:04 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,23 @@
 ## code your solution here. 
+class Cat
+  attr_reader :name
+  attr_accessor :meow
+  
+  def initialize()
+    @name=name
+  end
+  
+  def name=(new_name)
+    @name=new_name
+  end
+  
+ 
+  
+end
+
+maru = Cat.new
+maru.name = "Maru"
+maru.meow = "meow!"
+
+
+return maru.name
